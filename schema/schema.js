10df51ece8994beba9cf9cd1d0a1b40311223397
S David Kong <SDAVIDKONG@gmail.com>
2023-01-13 18:31:09 -0800

    added mongoose schema

--- schema/schema.js
+++ schema/schema.js
@@ -10,20 +10,20 @@ const {
   GraphQLList,
 } = graphql;
 
-var books = [
-  { name: "Ash", genre: "Trainer", id: "1", authorId: "1" },
-  { name: "Pikachu", genre: "Pokemon", id: "2", authorId: "2" },
-  { name: "Brock", genre: "Trainer", id: "3", authorId: "3" },
-  { name: "hahahlol", genre: "Just LOL", id: "4", authorId: "3" },
-  { name: "no wayyyyy", genre: "yah yah yah", id: "5", authorId: "2" },
-  { name: "The English Patient", genre: "Classics", id: "6", authorId: "3" },
-];
+// var books = [
+//   { name: "Ash", genre: "Trainer", id: "1", authorId: "1" },
+//   { name: "Pikachu", genre: "Pokemon", id: "2", authorId: "2" },
+//   { name: "Brock", genre: "Trainer", id: "3", authorId: "3" },
+//   { name: "hahahlol", genre: "Just LOL", id: "4", authorId: "3" },
+//   { name: "no wayyyyy", genre: "yah yah yah", id: "5", authorId: "2" },
+//   { name: "The English Patient", genre: "Classics", id: "6", authorId: "3" },
+// ];
 
-var authors = [
-  { name: "Boob", age: 44, id: "1" },
-  { name: "Wooter", age: 33, id: "2" },
-  { name: "Hazmat", age: 22, id: "3" },
-];
+// var authors = [
+//   { name: "Boob", age: 44, id: "1" },
+//   { name: "Wooter", age: 33, id: "2" },
+//   { name: "Hazmat", age: 22, id: "3" },
+// ];
 
 const BookType = new GraphQLObjectType({
   name: "Book",
@@ -34,7 +34,7 @@ const BookType = new GraphQLObjectType({
     author: {
       type: AuthorType,
       resolve(parent, args) {
-        return lodash.find(authors, { id: parent.authorId });
+        //return lodash.find(authors, { id: parent.authorId });
       },
     },
   }),
@@ -49,7 +49,7 @@ const AuthorType = new GraphQLObjectType({
     books: {
       type: new GraphQLList(BookType),
       resolve(parent, args) {
-        return lodash.filter(books, { authorId: parent.id });
+        //return lodash.filter(books, { authorId: parent.id });
       },
     },
   }),
@@ -62,26 +62,26 @@ const RootQuery = new GraphQLObjectType({
       type: BookType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return lodash.find(books, { id: args.id });
+        // return lodash.find(books, { id: args.id });
       },
     },
     author: {
       type: AuthorType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return lodash.find(authors, { id: args.id });
+        // return lodash.find(authors, { id: args.id });
       },
     },
     books: {
       type: new GraphQLList(BookType),
       resolve(parent, args) {
-        return books;
+        // return books;
       },
     },
     authors: {
       type: new GraphQLList(AuthorType),
       resolve(parent, args) {
-        return authors;
+        // return authors;
       },
     },
   },
