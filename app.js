60061add4dea499729ee31ff90fcb9f1b3f42ec6
S David Kong <SDAVIDKONG@gmail.com>
2023-01-15 15:35:07 -0800

    project is outdated and uses deprecated packages

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
