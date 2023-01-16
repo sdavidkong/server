52d1f0682e6d9cfd7acb417d043975c29061fb28
S David Kong <SDAVIDKONG@gmail.com>
2023-01-15 17:44:12 -0800

    added type defs and resolvers

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const { ApolloServer } = require("apollo-server");
+const { typeDefs } = require("./schema/type-defs");
 
-const server = new ApolloServer();
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(() => {
+  console.log("Your API is Running");
+});
